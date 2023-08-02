fbb0f51b06d43571d6bf046722be92cb50ccfeb0
oswaldosdutra <=>
2023-08-02 16:06:13 +0100

    unit tests created

---
+++ src/test/java/com/automate/withme/runners/Firstfeature_runner.java
@@ -0,0 +1,22 @@
+package com.automate.withme.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		features="src/test/java/com/automate/withme/Features",
+		glue="com.automate.withme.stepdefs",
+		strict=true,
+		monochrome=true,
+		plugin={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"html:target/cucumber-html-reports", 
+				"json:target/cucumber-html-reports/cucumber.json"}
+		,tags={"@Unittests"}
+		)
+
+public class Firstfeature_runner { // This is feature runner
+}
