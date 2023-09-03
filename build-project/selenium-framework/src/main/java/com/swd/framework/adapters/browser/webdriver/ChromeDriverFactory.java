093aae5ac2fd2c159408cb720733edf21d71b601
oswaldosdutra <=>
2023-09-03 18:07:48 +0100

    framework changed

--- build-project/selenium-framework/src/main/java/com/swd/framework/adapters/browser/webdriver/ChromeDriverFactory.java
+++ build-project/selenium-framework/src/main/java/com/swd/framework/adapters/browser/webdriver/ChromeDriverFactory.java
@@ -3,6 +3,7 @@ package com.swd.framework.adapters.browser.webdriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverFactory implements WebDriverFactory {
     /**
@@ -11,6 +12,10 @@ public class ChromeDriverFactory implements WebDriverFactory {
     @Override
     public WebDriver createWebDriver() {
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("headless");
+
+        return new ChromeDriver(options);
     }
 }
